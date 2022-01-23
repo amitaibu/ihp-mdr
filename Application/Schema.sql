6c549b848a2f899d29255debe2697fdb177912de
Amitai Burstein <amitai@gizra.com>
2022-01-23 14:17:20 +0200

    Add nurses

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -10,3 +10,16 @@ CREATE TABLE devices (
     last_used TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     numeric_id SERIAL NOT NULL UNIQUE
 );
+CREATE TABLE nurses (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    pin_code TEXT NOT NULL UNIQUE,
+    vid SERIAL NOT NULL UNIQUE,
+    authority_id UUID[] DEFAULT '{}' NOT NULL
+);
+CREATE TABLE authorities (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL UNIQUE
+);
+CREATE INDEX nurses_authority_id_index ON nurses (authority_id);
+ALTER TABLE nurses ADD CONSTRAINT nurses_ref_authority_id FOREIGN KEY (authority_id) REFERENCES authorities (id) ON DELETE NO ACTION;
